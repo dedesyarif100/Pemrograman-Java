9446f2441ec80f201daeccfe9c9f904109c32bcb
Dede Syarifudin <dedesyarif177@gmail.com>
2021-05-17 08:37:50 +0700

    Penambahan Fitur Increment ID Otomatis CRUD LinkedList

--- Case Java/CaseJava/src/LinkedListJava/LinkedListMhs2.java
+++ Case Java/CaseJava/src/LinkedListJava/LinkedListMhs2.java
@@ -3,17 +3,18 @@ package LinkedListJava;
 import java.util.Scanner;
 
 public class LinkedListMhs2 {
-    Integer id, nim, update;
+    // LinkedListMhs2 idNumber;
+    public Integer id, nim, update;
     Double uts, uas;
     String nama, alamat, skill;
     LinkedListMhs2 next, prev;
-
+    
     static Scanner integer = new Scanner(System.in);
     Scanner string = new Scanner(System.in);
     Scanner doubleValue = new Scanner(System.in);
-    public void inputData() {
-        System.out.print("Masukkan ID           : ");
-        id = integer.nextInt();
+    public void inputData(Integer idNumber) {
+        System.out.print("ID TERBUAT OTOMATIS   : " + (id = idNumber) + "\n");
+        id = idNumber;
         System.out.print("Masukkan Nim          : ");
         nim = integer.nextInt();
         System.out.print("Masukkan Nama         : ");
@@ -54,30 +55,50 @@ public class LinkedListMhs2 {
         selectData.uas = doubleValue.nextDouble();
     }
 
+    public void remove(LinkedListMhs2 selectData) {
+        selectData = null;
+    }
+
+    public LinkedListMhs2 getNext() {
+        return next;
+    }
+
+    public LinkedListMhs2 getPrev() {
+        return prev;
+    }
+
     public static void main(String[] args) {
-        int menu = 0, delete;
+        int menu = 0, id = 1, delete;
         LinkedList2 inputMenu = new LinkedList2();
         while (menu != 5 && menu <= 5) {
             System.out.print("1. Input\n2. View\n3. Update\n4. Delete\n5. Exit\n");
             System.out.print("Masukkan Menu : ");
             menu = integer.nextInt();
             if (menu == 1) {
-                inputMenu.add();
+                // idNumber = id;
+                System.out.println(id);
+                inputMenu.add(id);
+                id++;
                 System.out.println("Tambah Data Selesai");
                 System.out.println("-----------------------------------------\n");
             } else if (menu == 2) {
-                inputMenu.view();
+                inputMenu.view(id);
+                System.out.println("Cek Id Number : " + id);
             } else if (menu == 3) {
-                inputMenu.updateData();
+                inputMenu.updateData(id);
             } else if (menu == 4) {
-                System.out.print("\n1. Data Pertama\n2. Data Terakhir\n");
+                System.out.print("\n1. Data Pertama\n2. Data Terakhir\n3. Remove Berdasarkan ID\n");
                 System.out.print("Pilih Menu : ");
                 delete = integer.nextInt();
                 if (delete == 1) {
-                    inputMenu.removeFirst();
+                    inputMenu.removeFirst(id);
                 } else if (delete == 2) {
                     inputMenu.removeLast();
-                } else {
+                } 
+                // else if (delete == 3) {
+                //     inputMenu.removeByID();
+                // } 
+                else {
                     System.out.println("Batas Maksimal hapus sampai 2 angka");
                 }
             } else if (menu == 5) {
@@ -91,14 +112,15 @@ public class LinkedListMhs2 {
 
 class LinkedList2 {
     LinkedListMhs2 head, tail;
+    Integer id;
     public LinkedList2() { // Constructor
         head = null;
         tail = null;
     }
 
-    public void add() {
+    public void add(int idNumber) {
         LinkedListMhs2 addData = new LinkedListMhs2();
-        addData.inputData();
+        addData.inputData(idNumber);
         if (head == null) {
             head = addData;
         } else {
@@ -107,7 +129,8 @@ class LinkedList2 {
         tail = addData;
     }
 
-    public void view() {
+    public void view(int id) {
+        id = head.id;
         if (head == null) {
             System.out.println("Data Kosong");
             System.out.println("------------------------------------\n");
@@ -119,13 +142,15 @@ class LinkedList2 {
             while (viewData != null) {
                 viewData.viewData();
                 viewData = viewData.next;
+                id++;
             }
             System.out.println("View Data Selesai");
             System.out.println("-----------------------------------------\n");
+            System.out.println("Cek Id Number di View : " + id);
         }
     }
 
-    public void updateData() {
+    public void updateData(int id) {
         if (head == null) {
             System.out.println("Data Kosong");
             System.out.println("------------------------------------\n");
@@ -155,7 +180,7 @@ class LinkedList2 {
         }
     }
 
-    public void removeFirst() {
+    public void removeFirst(int id) {
         if (head == null) {
             System.out.println("Data Kosong");
             System.out.println("------------------------------------\n");
@@ -164,8 +189,11 @@ class LinkedList2 {
             System.out.println("| ID | NIM | Nama | Alamat | Skill | UTS | UAS |");
             System.out.println("------------------------------------");
             System.out.println("| "+ head.id +" | "+ head.nim +" | "+ head.nama +" | "+ head.alamat +" | "+ head.skill +" | "+ head.uts +" | "+ head.uas +" |");
-            System.out.println("Data "+ head.nama +" Berhasil Dihapus");
+            System.out.println("Data "+ head.id +" Berhasil Dihapus");
+            System.out.println("Cek Data Head.Next : " + head.next.id);
+            System.out.println("Cek Data Head : " + head.id);
             head = head.next;
+            System.out.println("Cek Data Head : " + head.id);
             System.out.println("Data Pertama Berhasil Dihapus");
             System.out.println("------------------------------------\n");
         }
@@ -180,7 +208,7 @@ class LinkedList2 {
             System.out.println("| ID | NIM | Nama | Alamat | Skill | UTS | UAS |");
             System.out.println("------------------------------------");
             System.out.println("| "+ tail.id +" | "+ tail.nim +" | "+ tail.nama +" | "+ tail.alamat +" | "+ tail.skill +" | "+ tail.uts +" | "+ tail.uas +" |");
-            System.out.println("Data "+ tail.nama +" Berhasil Dihapus");
+            System.out.println("Data "+ tail.id +" Berhasil Dihapus");
             head = null;
             tail = null;
             System.out.println("Data Kembali NULL");
@@ -189,7 +217,7 @@ class LinkedList2 {
             System.out.println("| ID | NIM | Nama | Alamat | Skill | UTS | UAS |");
             System.out.println("------------------------------------");
             System.out.println("| "+ tail.id +" | "+ tail.nim +" | "+ tail.nama +" | "+ tail.alamat +" | "+ tail.skill +" | "+ tail.uts +" | "+ tail.uas +" |");
-            System.out.println("Data "+ tail.nama +" Berhasil Dihapus");
+            System.out.println("Data "+ tail.id +" Berhasil Dihapus");
             System.out.println("Data Terakhir Berhasil Dihapus");
             System.out.println("------------------------------------\n");
             LinkedListMhs2 removeLastData = head.next;
@@ -202,4 +230,72 @@ class LinkedList2 {
             tail = backOff;
         }
     }
+    // public void removeByID() {
+    //     if (head == null) {
+    //         System.out.println("Data Kosong");
+    //         System.out.println("------------------------------------\n");
+    //     } else {
+    //         LinkedListMhs2 send = new LinkedListMhs2();
+    //         int save = send.inputIDupdate();
+    //         LinkedListMhs2 slow_ptr = head;
+    //         LinkedListMhs2 selectData = head;
+    //         LinkedListMhs2 tampung = null;
+    //         LinkedListMhs2 temp = null;
+    //         LinkedListMhs2 prev = null;
+    //         while (selectData != null) {
+    //             if (selectData.id == save) {
+    //                 System.out.println("| ID | NIM | Nama | Alamat | Skill | UTS | UAS |");
+    //                 System.out.println("------------------------------------");
+    //                 System.out.println("| "+ selectData.id +" | "+ selectData.nim +" | "+ selectData.nama +" | "+ selectData.alamat +" | "+ selectData.skill +" | "+ selectData.uts +" | "+ selectData.uas +" | ");
+    //                 System.out.println("Berhasil Masuk Pengujian");
+    //                 System.out.println("Remove Data BY ID : ");
+    //                 // ---------------------------------------------
+    //                 // temp = selectData; // temp = data ID save
+    //                 // prev = slow_ptr; // prev = head
+    //                 // slow_ptr = slow_ptr.next; // head = head.next
+    //                 // break;
+    //                 // ---------------------------------------------
+    //                 // temp = head;
+    //                 // // head = selectData;
+    //                 // prev = selectData.next;
+    //                 // break;
+    //                 // ---------------------------------------------
+    //                 // temp = head;
+    //                 // temp = selectData;
+    //                 // prev = selectData.next;
+    //                 // break;
+    //                 // ---------------------------------------------
+    //                 // temp = selectData;
+    //                 // selectData = null;
+    //                 // selectData = temp.next;
+    //                 // System.out.println(selectData.id);
+
+    //                 // selectData = selectData.next;
+    //                 // temp = null;
+    //             } else {
+    //                 tampung = selectData;
+    //                 selectData = selectData.next;
+    //                 // System.out.println(selectData.id);
+    //             }
+    //         }
+    //         // System.out.println(head.next.id); // id 2
+    //         // head.next = slow_ptr.next; // Fungsi Hapus Sudah Ketemu
+    //         // System.out.println(head.next.id); // id 3
+    //         // System.out.println(slow_ptr.next.id); // id 2
+    //         // ---------------------------------------------------------
+    //         // head = selectData;
+    //         // head = tampung, prev;
+    //         // tail = head;
+    //         // head = tampung.getPrev();
+    //         // System.err.println(head.id);
+    //         // System.out.println(tail.id);
+    //         // head.prev = temp;
+    //         // System.out.println(head.prev.id);
+    //         // ---------------------------------------------------------
+    //         // System.out.println(temp.id);
+    //         // temp = prev;
+    //         // System.out.println(prev.id);
+    //         // selectData = prev;
+    //     }
+    // }
 }
